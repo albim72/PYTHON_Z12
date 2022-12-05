5ac75a91d904815c4358ec1510035ed9d9b49c5a
albim72 <marcin.albiniak72@gmail.com>
2022-12-05 13:37:46 +0100

    Update generatory.py

--- DZIEN_1/generatory.py
+++ DZIEN_1/generatory.py
@@ -7,3 +7,27 @@ def liczby():
 print(liczby())
 for p in liczby():
     print(p)
+
+#przypadek 2
+
+def wznowienie(n,k):
+    print("wstrzymanie działania....")
+    yield 1001
+    print("wznowienie działania....")
+
+    print("wstrzymanie działania....")
+    yield n+k
+    print("wznowienie działania....")
+
+    print("wstrzymanie działania....")
+    yield n-k
+    print("wznowienie działania....")
+
+    print("wstrzymanie działania....")
+    yield n*k
+    print("wznowienie działania....")
+
+for i in wznowienie(6,8):
+    if i == 1001:
+        continue
+    print(f'zwrócono wartość: {i}')

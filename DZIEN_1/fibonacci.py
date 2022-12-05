50dffe42d2405f7e16127fc18aec3d3fa972111f
albim72 <marcin.albiniak72@gmail.com>
2022-12-05 15:19:07 +0100

    Update fibonacci.py

--- DZIEN_1/fibonacci.py
+++ DZIEN_1/fibonacci.py
@@ -1,6 +1,12 @@
+def trace(funkcja):
+    def wrapper(*args,**kwargs):
+        wynik = funkcja(*args,**kwargs)
+        print(f'{funkcja.__name__}({args!r},{kwargs!r})'
+              f'->{wynik!r}')
+        return wynik
+    return wrapper
 
-
-
+@trace
 def fibonacci(n):
     if n in (0,1):
         return n

0376a5468dc8939113f7e3fd4aecadc2c682b018
albim72 <marcin.albiniak72@gmail.com>
2022-12-05 13:51:15 +0100

    Update statystyki.py

--- DZIEN_1/statystyki.py
+++ DZIEN_1/statystyki.py
@@ -1,1 +1,18 @@
 liczby = [6,78,90,1001,-999,34,0,178,199,987,-1090,1213,66,3,4,1,90,35,2]
+
+def statystyki(dane):
+    minimum = min(dane)
+    maksimum = max(dane)
+    rozstep = maksimum-minimum
+    n = len(dane)
+    suma = sum(dane)
+    avg = suma/n
+    return minimum,maksimum,rozstep,n,suma,avg
+
+wynik = statystyki(liczby)
+print(type(wynik))
+print(wynik)
+
+mini,maxi,roz,n,sm,av = statystyki(liczby)
+print(f'wartość największa: {maxi}, wartość najmniejsza: {mini}, rozstęp wartości: {roz},'
+      f' liczba elementów: {n}, suma elementów: {sm}, średnia arytmetyczna: {av:.2f}')

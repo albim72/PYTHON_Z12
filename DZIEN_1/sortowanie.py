84653c3e58b9799778b2808234525f5dcb37379c
albim72 <marcin.albiniak72@gmail.com>
2022-12-05 14:40:27 +0100

    Create sortowanie.py

---
+++ DZIEN_1/sortowanie.py
@@ -0,0 +1,6 @@
+zestaw_classic = ['samolot','aktor','miecz','mecz','Mieczysław','Gdańsk','anakonda','zenit',
+                  'zakamarki','piorun','atak','macierz']
+
+
+zestaw_modern = ['samolot','aktor','miecz','mecz','Mieczysław','Gdańsk','anakonda','zenit',
+                  'zakamarki','piorun','atak','macierz']

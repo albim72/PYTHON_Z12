266e4ca8ef6c9012348972a580b88b992d72fa69
albim72 <marcin.albiniak72@gmail.com>
2022-12-05 11:37:52 +0100

    Update lambda.py

--- DZIEN_1/lambda.py
+++ DZIEN_1/lambda.py
@@ -15,3 +15,20 @@ def policz(n,m):
 print(policz(9,3)(4))
 
 nb = [56,112,0,-89,3,7,9,11,101,-90,12,-44]
+
+
+#stwórz nową listę o nazwie parzyste i wypełnij ją wszystkim parzystymi wartościami z listy nb
+#funkcja filter(funkcja,dane) -> funckcja -> f. przekazująca warunek, dane do filtrowania
+
+parzysta = list(filter(lambda x:x%2==0,nb))
+print(parzysta)
+
+def warunek(x):
+    return x%2==0
+
+parzysta = list(filter(warunek,nb))
+print(parzysta)
+
+#stwórz nową listę cube i wypełnij ją wszytskimi wartościami z listy nb podniesionymi do potęgi trzeciej
+cube = list(map(lambda x:x**3,nb))
+print(cube)

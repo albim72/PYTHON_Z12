2329eae9c7d5d0f2e59d39a765e737a2c5d3b468
albim72 <marcin.albiniak72@gmail.com>
2022-12-06 09:26:16 +0100

    Update main.py

--- DZIEN_2/OSOBY_/main.py
+++ DZIEN_2/OSOBY_/main.py
@@ -1,4 +1,5 @@
 from osoba import Osoba
+from pracownik import Pracownik
 
 za = 12
 print("________________ osoba ______________")
@@ -12,3 +13,10 @@ os2 = Osoba("Olga",23,51,163)
 os2.print_osoba()
 print(f'wiek za {za} lat wynosi: {os2.wiek_za_x_lat(za)}')
 print(f'czy osoba jest pracownikiem? ({os2.czypracownik()})')
+
+print("_____________pracownik_____________")
+pr1 = Pracownik("Karol",54,102,175,"ABC","dyrektor",12,10340)
+pr1.print_osoba()
+pr1.print_pracownik()
+print(f'wiek za {za} lat wynosi: {pr1.wiek_za_x_lat(za)}')
+print(f'czy osoba jest pracownikiem? ({pr1.czypracownik()})')

eace99c9ea07f02bd3f7db1453d3652b6b9910bb
albim72 <marcin.albiniak72@gmail.com>
2022-12-06 09:07:25 +0100

    Create osoba.py

---
+++ DZIEN_2/OSOBY_/osoba.py
@@ -0,0 +1,21 @@
+class Osoba:
+    def __init__(self,imie,wiek,waga,wzrost):
+        self.imie = imie
+        self.wiek = wiek
+        self.waga = waga
+        self.wzrost = wzrost
+        self.pracownik = False
+        self.info()
+        
+    def info(self):
+        print("utworzono nową osobę...")
+        
+    def print_osoba(self):
+        print(f'osoba -> imię: {self.imie}, wiek: {self.wiek}, waga: {self.waga} kg, '
+              f'wzrost: {self.wzrost} cm')
+        
+    def wiek_za_x_lat(self,x):
+        return self.wiek + x
+    
+    def czypracownik(self):
+        return self.pracownik

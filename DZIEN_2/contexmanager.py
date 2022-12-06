3d5041bbe1603694b0d57b6fe1208b411364e8be
albim72 <marcin.albiniak72@gmail.com>
2022-12-06 15:48:03 +0100

    Create contexmanager.py

---
+++ DZIEN_2/contexmanager.py
@@ -0,0 +1,13 @@
+class ContextManager:
+    def __init__(self):
+        print('inicjacja metody init()')
+
+    def __enter__(self):
+        print('inicjacja metody enter()')
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        print('inicjacja metody exit()')
+
+
+with ContextManager() as manager:
+    print('część wykonawcza bloku with....')

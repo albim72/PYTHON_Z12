8021c21900d769a3196fe8060b76f5b0e11c0302
albim72 <marcin.albiniak72@gmail.com>
2022-12-06 11:29:25 +0100

    Update main.py

--- DZIEN_2/REZYSTANCJA/main.py
+++ DZIEN_2/REZYSTANCJA/main.py
@@ -1,7 +1,16 @@
 from oldresistor import OldResistor
+from resistor import Resistor
 
 r0 = OldResistor(2.2E3)
 print(f"_________________{r0.__class__.__name__}____________________")
 print(r0.get_ohms())
 r0.set_ohms(1.1E2)
 print(r0.get_ohms())
+
+r1 = Resistor(2.1E2)
+print(f"_________________{r1.__class__.__name__}____________________")
+print(f'oporność początkowa: {r1.ohms} om')
+r1.ohms = 7.8E2
+
+print(f"układ elektryczny:\noporność: {r1.ohms} om\nnapięcie prądu: {r1.voltage} V\n"
+      f"natężenie prądu: {r1.current} A")

764931fab28694c8a8d8fb6cb1cadbb04f4f2f0a
albim72 <marcin.albiniak72@gmail.com>
2022-12-06 09:18:23 +0100

    Update osoba.py

--- DZIEN_2/OSOBY_/osoba.py
+++ DZIEN_2/OSOBY_/osoba.py
@@ -4,18 +4,19 @@ class Osoba:
         self.wiek = wiek
         self.waga = waga
         self.wzrost = wzrost
+        self.kolor_oczu = "brązowe"
         self.pracownik = False
         self.info()
-        
+
     def info(self):
         print("utworzono nową osobę...")
-        
+
     def print_osoba(self):
         print(f'osoba -> imię: {self.imie}, wiek: {self.wiek}, waga: {self.waga} kg, '
-              f'wzrost: {self.wzrost} cm')
-        
+              f'wzrost: {self.wzrost} cm, kolor oczu: {self.kolor_oczu}')
+
     def wiek_za_x_lat(self,x):
         return self.wiek + x
-    
+
     def czypracownik(self):
         return self.pracownik

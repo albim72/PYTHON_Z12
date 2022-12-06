44744ea8dc577dac1348da5fc0709b268e00d806
albim72 <marcin.albiniak72@gmail.com>
2022-12-06 11:13:44 +0100

    Update konstrukcja.py

--- DZIEN_2/konstrukcja.py
+++ DZIEN_2/konstrukcja.py
@@ -1,3 +1,5 @@
+
+print("***** przypadek 1 *********")
 class Pojazd:
     #dunder
     #double underline
@@ -8,6 +10,31 @@ class Pojazd:
     def __init__(self,name):
         print(f'inicjowanie obiektu klasy Pojazd...')
         self.name = name
-        
+
 
 poj = Pojazd("Landrover")
+
+print("***** przypadek 2 *********")
+class KwadratLiczby(int):
+    def __new__(cls, wart):
+        return super().__new__(cls,wart**2)
+
+wx = KwadratLiczby(4)
+print(wx)
+
+
+print("***** przypadek 3 *********")
+class Pacjent:
+    def __new__(cls, imie,nazwisko):
+        obj = super().__new__(cls)
+
+        obj.imie = imie
+        obj.nazwisko = nazwisko
+
+        obj.pelne_dane = f'{imie} {nazwisko}'
+        return obj
+
+pc = Pacjent('Jan','Kowalski')
+print(pc.pelne_dane)
+print(pc.__dict__)
+

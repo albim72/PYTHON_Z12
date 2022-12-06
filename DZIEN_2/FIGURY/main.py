09e6f4618c5ba1a43eae27bb2ec7ce5878e1cc3e
albim72 <marcin.albiniak72@gmail.com>
2022-12-06 14:54:24 +0100

    Update main.py

--- DZIEN_2/FIGURY/main.py
+++ DZIEN_2/FIGURY/main.py
@@ -1,6 +1,7 @@
 from prostokat import  Prostokat
 from trojkat import Trojkat
 from trapez import Trapez
+from kolo import Kolo
 
 pr = Prostokat(5.5,7.8)
 print(f'Pole figury: {pr.__class__.__name__} wynosi: {pr.policzpole():.2f} cm2')
@@ -10,3 +11,7 @@ print(f'Pole figury: {tr.__class__.__name__} wynosi: {tr.policzpole():.2f} cm2')
 
 trp = Trapez(6.3,4.8,4.1)
 print(f'Pole figury: {trp.__class__.__name__} wynosi: {trp.policzpole():.2f} cm2')
+
+#stwórz klasę Kolo(Figura) i policz pole koła dla promienia 5.5
+kl = Kolo(5.5)
+print(f'Pole figury: {kl.__class__.__name__} wynosi: {kl.policzpole():.2f} cm2')

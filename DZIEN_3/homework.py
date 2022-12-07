83fb79038e14f13487323fd16dc97711622c2a84
albim72 <marcin.albiniak72@gmail.com>
2022-12-07 10:39:01 +0100

    Update homework.py

--- DZIEN_3/homework.py
+++ DZIEN_3/homework.py
@@ -15,3 +15,39 @@ class Homework:
 galileo = Homework()
 galileo.grade = 98
 assert galileo.grade == 98
+
+class Exam:
+    def __init__(self):
+        self._writing_grade = 0
+        self._math_grade = 0
+
+    @staticmethod
+    def _check_grade(value):
+        if not (0<=value<=100):
+            raise ValueError('ocena musi zawierać się w zakresie: 0-100')
+
+    @property
+    def writing_grade(self):
+        return self._writing_grade
+
+    @writing_grade.setter
+    def writing_grade(self,value):
+        self._check_grade(value)
+        self._writing_grade = value
+
+    @property
+    def math_grade(self):
+        return self._math_grade
+
+    @math_grade.setter
+    def math_grade(self, value):
+        self._check_grade(value)
+        self._math_grade = value
+
+
+albert = Exam()
+albert.writing_grade = 82
+albert.math_grade = 76
+
+assert albert.writing_grade == 82
+assert albert.math_grade == 76

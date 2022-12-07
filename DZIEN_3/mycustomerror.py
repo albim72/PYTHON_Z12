06398f6ae3edca138b92804b5d5a4af9aedcff68
albim72 <marcin.albiniak72@gmail.com>
2022-12-07 08:14:00 +0100

    Update mycustomerror.py

--- DZIEN_3/mycustomerror.py
+++ DZIEN_3/mycustomerror.py
@@ -3,11 +3,21 @@ class MyCustomError(Exception):
         if args:
             self.message = args[0]
         else:
-            self.message = None 
-            
+            self.message = None
+
     def __str__(self):
         print("wywołanie funkcji __str__")
         if self.message:
             return f'klasa {self.__class__.__name__} -> {self.message}'
         else:
             return f'klasa {self.__class__.__name__} została użyta'
+
+
+try:
+    n = 0
+
+    if n==0:
+        raise MyCustomError("mamy duży problem bo n=0")
+except MyCustomError as mc:
+    print(mc)
+    

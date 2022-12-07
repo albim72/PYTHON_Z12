181b40386fc73336ca6b445d61e9b7573dece934
albim72 <marcin.albiniak72@gmail.com>
2022-12-07 12:45:28 +0100

    Update osoba_dc.py

--- DZIEN_3/osoba_dc.py
+++ DZIEN_3/osoba_dc.py
@@ -18,3 +18,20 @@ print(p1)
 print(p1.age)
 
 #inne spojrzenie....
+#Field -> ('default','default_factory','init','repr','hash','compare','metadata','kw_only')
+
+params = {
+    'name':Field(None,None,True,True,True,True,None,None),
+    'surname':Field(None,None,True,True,True,True,None,None),
+    'year_birth':Field(None,None,True,True,True,True,None,None)
+
+}
+
+def age(self):
+    return datetime.now().year - self.year_birth
+#metadefinicja klasy
+Person = dataclass(type('Person',(),{'__annotations__':params,'age':property(age)}))
+
+pr = Person('Romulada','Tracz',1965)
+print(pr)
+print(pr.age)

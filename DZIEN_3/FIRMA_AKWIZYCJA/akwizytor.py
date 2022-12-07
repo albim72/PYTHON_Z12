f26afe38115764e0a66d4e07d0ed5975323e63f1
albim72 <marcin.albiniak72@gmail.com>
2022-12-07 12:15:00 +0100

    Update akwizytor.py

--- DZIEN_3/FIRMA_AKWIZYCJA/akwizytor.py
+++ DZIEN_3/FIRMA_AKWIZYCJA/akwizytor.py
@@ -1,24 +1,24 @@
 from decimal import Decimal
 
 class Akwizytor:
-    
+
     """
     pracownik, którego wynagrodzenie jest wyłącznie prowizją od sprzedaży
     """
-    
+
     def __init__(self,imie,nazwisko,nr_ubezp,sprzedaz,prowizja):
         self._imie = imie
         self._nazwisko = nazwisko
         self._nr_ubezp = nr_ubezp
         self._sprzedaz = sprzedaz
         self._prowizja = prowizja
-        
+
     @property
     def imie(self):
         return self._imie
 
     @property
-    def nazwiko(self):
+    def nazwisko(self):
         return self._nazwisko
 
     @property
@@ -38,25 +38,24 @@ class Akwizytor:
     @property
     def prowizja(self):
         return self._prowizja
-    
+
     @prowizja.setter
     def prowizja(self,procent):
         if not(Decimal('0.0')<procent<Decimal('30.0')):
             raise ValueError('prowizja musi zawierać się w przedziale od 0 do 30 procent')
         self._prowizja = procent
-        
+
     def zarobek(self):
         return self.sprzedaz*(self.prowizja/Decimal('100.0'))
-    
+
     def __repr__(self):
         """
         reprezentacja tesktowa obiektu
-        :return: 
+        :return:
         """
         return f'Akwizytor: {self.imie} {self.nazwisko}\n numer ubezpieczenia {self.nr_ubezp}\n' \
                f'prowizja: {self.prowizja} %\n' \
                f'sprzedaz: {self.sprzedaz} zł'
-        
 
-    
-    
+
+

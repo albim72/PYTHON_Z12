61657c773264a8ac7f2c6f21ad5f36f8c2343dad
albim72 <marcin.albiniak72@gmail.com>
2022-12-07 08:18:43 +0100

    Create slownik_error.py

---
+++ DZIEN_3/slownik_error.py
@@ -0,0 +1,6 @@
+#napisz klasę składającą słownik w oparciu o dwie listy lub krotki z kluczami i wartościami,
+#dodatkowo dowzolone typy wartości to: int i float
+#obsłuż potencjalne błędy wynikające z zadanych ograniczeń -> napisz dwie klasy wyjątków:
+#InFloatValueError -> reakcja na zadaną wartość inną niż int i float
+#KeyValueConstructError -> reakcja na niewłaściwą kolekcję definiującą klucz lub wartość słownika ->
+#inna niż krotka lub lista....

81bd4c73d94e1c6e502dfdcf06a0e63f49128e82
albim72 <marcin.albiniak72@gmail.com>
2022-12-08 14:52:08 +0100

    Update sqlite_conn.py

--- DZIEN_4/SQLITE/sqlite_conn.py
+++ DZIEN_4/SQLITE/sqlite_conn.py
@@ -5,10 +5,6 @@ def create_connection(db_file):
     conn = None
     try:
         conn = sqlite3.connect(db_file)
-        print(sqlite3.version)
     except Error as e:
         print(e)
-    finally:
-        if conn:
-            conn.close()
-            
+    return conn

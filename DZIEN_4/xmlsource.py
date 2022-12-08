53f309c7114d4bc6fb97a93be64b1d1ad679d3b2
albim72 <marcin.albiniak72@gmail.com>
2022-12-08 10:49:53 +0100

    Update xmlsource.py

--- DZIEN_4/xmlsource.py
+++ DZIEN_4/xmlsource.py
@@ -7,5 +7,10 @@ try:
     for child in root:
         print(f"tag: {child.tag}, atrybuty: {child.attrib}")
 
+    print(root[0])
+    print(root[0][2])
+    print(root[0][2].text)
+
 except:
     print("element nie isitnieje!")
+    raise

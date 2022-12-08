cd34007b681713fd462aa6b7b4524d59a2ccbc21
albim72 <marcin.albiniak72@gmail.com>
2022-12-08 08:28:46 +0100

    Update urls.py

--- DZIEN_4/urls.py
+++ DZIEN_4/urls.py
@@ -10,3 +10,20 @@ URLS = [
     "https://mil.ru/"
 
 ]
+
+def load_url(url,timeout):
+    with urllib.request.urlopen(url,timeout=timeout) as conn:
+        return conn.read()
+
+
+with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+    future_to_url = {executor.submit(load_url,url,60):url for url in URLS}
+    for future in concurrent.futures.as_completed(future_to_url):
+        url = future_to_url[future]
+
+        try:
+            data = future.result()
+        except Exception as ext:
+            print(f"{url} generuje wyjątek: {ext}")
+        else:
+            print(f"{url} - strona ma rozmiar: {len(data)} bajtów")

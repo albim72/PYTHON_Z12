e3698be91ed07ab488f3604a017ff8d383086477
albim72 <marcin.albiniak72@gmail.com>
2022-12-08 09:04:29 +0100

    Create queue_stack.py

---
+++ DZIEN_4/queue_stack.py
@@ -0,0 +1,33 @@
+queue = []
+queue.append('mors')
+queue.append('foka')
+queue.append('orka')
+
+print("inicjacja kolejki")
+print(queue)
+
+print("elementy zdjęte z kolejki\n")
+print(queue.pop(0))
+print(queue.pop(0))
+print(queue.pop(0))
+
+print("kolejka po zdjęciu elementów")
+print(queue)
+
+#stos
+print("_______________________________________")
+
+stack = []
+stack.append('mors')
+stack.append('foka')
+stack.append('orka')
+
+print("inicjacja stosu")
+print(stack)
+print("elementy zdjęte ze stosu\n")
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
+
+print("stos po zdjęciu elementów")
+print(stack)

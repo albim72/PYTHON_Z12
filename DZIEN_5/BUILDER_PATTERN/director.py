2b2b2f2921f252b8d5f83582d3941441e283b6ec
albim72 <marcin.albiniak72@gmail.com>
2022-12-09 09:07:50 +0100

    Create director.py

---
+++ DZIEN_5/BUILDER_PATTERN/director.py
@@ -0,0 +1,21 @@
+from builder import Builder
+
+class Director:
+    def __init__(self)->None:
+        self._builder = None
+        
+    @property
+    def builder(self)->Builder:
+        return self._builder
+    
+    @builder.setter
+    def builder(self,builder:Builder)->None:
+        self._builder = builder
+        
+    def build_minimal_variable_product(self)->None:
+        self.builder.product_part_a()
+        
+    def build_full_featured_product(self)->None:
+        self.builder.product_part_a()
+        self.builder.product_part_b()
+        self.builder.product_part_c()

199bbd367aceb8490733865cc04e5d3fe7e42508
albim72 <marcin.albiniak72@gmail.com>
2022-12-09 10:56:43 +0100

    Update colors.py

--- DZIEN_5/colors.py
+++ DZIEN_5/colors.py
@@ -18,5 +18,12 @@ async def makerandom(idx:int,treshold:int=6) -> int:
     print(c[idx + 1] + f"--> koniec: makerandom({idx}) == {i} {c[0]}")
     return i
 
+async def main():
+    res = await asyncio.gather(*(makerandom(i,7) for i in range(3)))
+    return res
 
-        
+if __name__ == '__main__':
+    random.seed(444)
+    r1,r2,r3 = asyncio.run(main())
+    print()
+    print(f"r1:{r1}, r2: {r2}, r3: {r3}")

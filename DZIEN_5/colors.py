815dc1317fe2781493e74b823ac918f1d519599e
albim72 <marcin.albiniak72@gmail.com>
2022-12-09 10:49:24 +0100

    Update colors.py

--- DZIEN_5/colors.py
+++ DZIEN_5/colors.py
@@ -7,3 +7,16 @@ c =(
     "\033[91m", #Czerwony
     "\033[35m" #Magenta
 )
+
+async def makerandom(idx:int,treshold:int=6) -> int:
+    print(c[idx+1] + f"inicjacja funkcji makerandom({idx}).")
+    i = random.randint(0,10)
+    while  i <= treshold:
+        print(c[idx+1] + f"makerandom({idx}) == {i} zbyt mało, powtórz!")
+        await asyncio.sleep(idx+1)
+        i = random.randint(0,10)
+    print(c[idx + 1] + f"--> koniec: makerandom({idx}) == {i} {c[0]}")
+    return i
+
+
+        
